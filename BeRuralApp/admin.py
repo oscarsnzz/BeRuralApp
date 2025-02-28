5e3c968084e4e767fec7c89143e13533b371a12c
Oscar Sánchez Vidal <128138623+oscarsnzz@users.noreply.github.com>
2025-02-28 19:00:08 +0100

    Configuración de los modelos en admin.py

--- BeRuralApp/admin.py
+++ BeRuralApp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Usuario, Municipio, Repoblador, Ayuntamiento, Incentivo, Valoracion, Oportunidad, Mensaje
 
-# Register your models here.
+admin.site.register(Usuario)
+admin.site.register(Municipio)
+admin.site.register(Repoblador)
+admin.site.register(Ayuntamiento)
+admin.site.register(Incentivo)
+admin.site.register(Valoracion)
+admin.site.register(Oportunidad)
+admin.site.register(Mensaje)
