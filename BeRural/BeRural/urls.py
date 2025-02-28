f044706cf90ebc84b83bea12fa1bd61a671a4918
Oscar Sánchez Vidal <128138623+oscarsnzz@users.noreply.github.com>
2025-02-28 19:14:44 +0100

    m

--- BeRural/BeRural/urls.py
+++ BeRural/BeRural/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('beruralapp.urls')),  # Incluye las URLs de la app
+    path('', include('BeRural.urls')),  # Incluye las URLs de la app
 ]
